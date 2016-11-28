61d30db2665c9fd67e7e916766280fcef49eee68
Steven McDonald <stevenmcdonald@Stevens-iMac.local>
2016-11-28 17:29:41 -0600

    Initial commit

---
+++ src/main/java/com/lagacyfit/LegacyFitApplication.java
@@ -0,0 +1,12 @@
+package com.lagacyfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegacyFitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegacyFitApplication.class, args);
+	}
+}
---
+++ src/test/java/com/lagacyfit/LegacyFitApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lagacyfit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LegacyFitApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
